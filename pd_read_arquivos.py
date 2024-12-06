1dd072e8f54e0ebdcf2116add3aba8985386a036
GHAS06 <guilherme.silva@claversystems.net>
2024-12-06 13:33:37 -0300

    Especificando leitura de arquivos com pandas

--- pd_read_arquivos.py
+++ pd_read_arquivos.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
 # ler arquivo csv
-df = pd.read_csv(r"C:\Estudando_pandas\Arquivos_csv_estudos\202401_Compras.csv",encoding='latin1',sep = ";", header=0, nrows=10, usecols=["Número do Contrato","Objeto","Fundamento Legal"])
+#df = pd.read_csv(r"C:\Estudando_pandas\Arquivos_csv_estudos\202401_Compras.csv",encoding='latin1',sep = ";", header=0, nrows=10, usecols=["Número do Contrato","Objeto","Fundamento Legal"])
 
 # ler arquivos excel
-#df = pd.read_excel("/caminho/arquivo.xlsx")
+#df = pd.read_excel(r"C:\Estudando_pandas\Arquivos_excel_para_estudos\Relatorio DER Novembro2024.xlsx", sheet_name= 0)
+
+#arquivo_excel = pd.ExcelFile(r"C:\Estudando_pandas\Arquivos_excel_para_estudos\Relatorio DER Novembro2024.xlsx")
+
+#print(arquivo_excel.sheet_names)
+
+#ABA1 = arquivo_excel.parse("Multas-Novembro")
+#ABA2 = arquivo_excel.parse("Protocolo-Novembro")
+
+#print(ABA1, ABA2)
 
 # ler arquivos json,
 #df = pd.read_json("/caminho/arquivo.json")
@@ -24,4 +33,4 @@ query = "SELECT * FROM sua_tabela"
 df_sql = pd.read_sql(query, engine) # Exibir o DataFrame
 '''
 #print(df.info())
-print(df)
+#print(df)
