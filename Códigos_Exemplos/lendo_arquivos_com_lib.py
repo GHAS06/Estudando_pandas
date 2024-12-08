1d053d80061dc464a84419e2d1e2263904bc7cad
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-12-08 13:51:43 -0300

    Aprendendo outras formas de abrir arquivos com pandas

---
+++ Códigos_Exemplos/lendo_arquivos_com_lib.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+from pathlib import Path
+
+'''
+# Diretório base
+diretorio_base = os.getcwd()
+# o diretório atual é a pasta Estudando_pandas, com isso consigo contruir um caminnho 
+
+# Construindo o caminho correto
+caminho_arquivo = os.path.join(diretorio_base, 'Arquivos_csv_estudos', '202401_ItemCompra.csv')
+
+# Verifique o caminho gerado
+print("Caminho do arquivo:", caminho_arquivo)
+
+# Tente carregar o arquivo
+try:
+    df = pd.read_csv(caminho_arquivo, encoding= "latin1", sep =';')
+    print(df.head())
+
+except FileNotFoundError as e:
+    print(f"Erro: {e}")
+'''
+
+'''
+# usando o pathlib para realizar a mesma coisa acima
+diretório_atual = Path.cwd()
+
+Construindo_caminho = Path.joinpath(diretório_atual, 'Arquivos_csv_estudos', '202401_ItemCompra.csv')
+
+try:
+    df = pd.read_csv(Construindo_caminho, encoding='latin1',sep=';')
+    print(df)   
+except Exception as e:
+    print(f'Ocorreu um erro): {e}')
+'''
+
+# usando os dois, tanto o os e o pathlib
+
+diretório_base = Path(os.getcwd())
+
+caminho_construido = diretório_base.joinpath('Arquivos_csv_estudos', '202401_ItemCompra.csv')
+
+#print(str(caminho_construido))
+
+try:
+    df = pd.read_csv(caminho_construido,encoding='latin1',sep=';')  
+    print(df)
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
