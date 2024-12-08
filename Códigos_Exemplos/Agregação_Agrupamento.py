4644a76e422c152450c9c28403a580a976cb7082
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-12-08 11:29:05 -0300

    APROFUNDANDO SOBRE GROUPBY E FUNÇÕES DE AGREGAMENTO/CONCERTANDO DOCUMENTAÇÃO

--- Códigos_Exemplos/Agregação_Agrupamento.py
+++ Códigos_Exemplos/Agregação_Agrupamento.py
@@ -31,8 +31,8 @@ colunas = [
 filtro = df[(df['Código Órgão'] == 26290) & (df['Quantidade Item'] >= 2)].sort_values(['Quantidade Item'], ascending=False)
 
 # Selecionando colunas específicas com iloc
-resultado = filtro.iloc[:, [7,8,9]] 
-#print(resultado)
+resultado = filtro.iloc[:, [0,1,2,8,9]] 
+print(resultado)
 
 # Usando a lista 'colunas' para selecionar as colunas pelos nomes (como uma sublista)7
 # usando o loc
@@ -44,13 +44,17 @@ Por que o : sozinho?
 O Pandas precisa saber se você está se referindo às linhas ou colunas do DataFrame.
 
 Antes da vírgula (:): Significa todas as linhas.
-Depois da vírgula (:): Significa todas as colunas.
+Depois da vírgula (:): Significa todas as colunas.  
 Se você usar apenas o : em uma das posições, o Pandas entende que deseja "tudo" naquela dimensão.
 '''
 
 # USANDO O GROUPBY para agregar e agrupar
-df_agrupado = df[['Código Órgão','Quantidade Item']].groupby('Código Órgão').sum()
-print(df_agrupado)
+
+#resultado_agrupado = resultado[['Código Órgão','Quantidade Item']].groupby(['Código Órgão']).sum()
+#print(resultado_agrupado)
+
+#df_agrupado = df[['Código Órgão','Quantidade Item']].groupby('Código Órgão').sum()
+#print(df_agrupado)
 
 '''
 Este código está agrupando os dados pelo 'Código Órgão' e somando os valores da coluna
